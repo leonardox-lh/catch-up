9976052800274431aa388b82fb7a56a114541c08
leonardo <u202124304@upc.edu.pe>
2023-09-11 21:05:57 -0500

    feat(article): added article entity .

---
+++ src/app/news/model/article.entity.ts
@@ -0,0 +1,19 @@
+import {Source} from "./source.entity";
+export class Article {
+  title:string;
+  description: string;
+  url: string;
+  urlToImage:string;
+  source:Source;
+
+  constructor() {
+    this.title='';
+    this.description='';
+    this.url='';
+    this.urlToImage='';
+    this.source=new Source();
+  }
+}
+
+export class ArticleEntity {
+}
--- src/app/news/model/source.entity.ts
+++ src/app/news/model/source.entity.ts
@@ -11,3 +11,6 @@ export class Source {
     this.urlToLogo='';
   }
 }
+
+export class SourceEntity {
+}
